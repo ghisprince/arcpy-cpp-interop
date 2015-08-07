a82efc7c824253b75be643e77956840965e4a5ad
ghis3080 <gprince@esri.com>
2015-08-07 15:00:51 -0700

    better error message

--- gptool_script.py
+++ gptool_script.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 This script shows
 
 #1: communication between python & dll
@@ -16,12 +16,29 @@ autoCancelling/isCancelled, but code below will work with older
 versions as well.
 """
 
-
 import arcpy
 import platform
 import ctypes
 import os
+import sys
 
+
+# path to dll (based on MS Visual Studio 2013)
+# this path will vary based on compiler settings
+# so update the line below
+#  - x32 vs x64
+#  - Release vs Debug
+
+dll_path = r"my-lib\Release\my-lib.dll"
+if platform.architecture()[0] == "64bit":
+    dll_path = dll_path.replace("Release", "x64\\Release")
+
+dll_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), dll_path)
+
+if not os.path.isfile(dll_path):
+  arcpy.AddError("Could not find dll {}".format(dll_path))
+  arcpy.AddError("Update gp_script.py line 32")
+  sys.exit(0)
 
 
 if hasattr(arcpy.env, "autoCancelling"):
@@ -51,28 +68,20 @@ class Callback(object):
         # return to dll IsCancelled (1 = user clicked cancel)
         return int(arcpy.env.isCancelled)
 
-# path to dll (based on MS Visual Studio 2013)
-dll_path = r"my-lib\Release\my-lib.dll"
-if platform.architecture()[0] == "64bit":
-    dll_path = dll_path.replace("Release", "x64\\Release")
+loaded_dll = ctypes.cdll.LoadLibrary(dll_path)
 
-loaded_dll = ctypes.cdll.LoadLibrary(
-           os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                dll_path))
-
-# Describe function being called in the dll 
+# Describe function being called in the dll
 lib_func = loaded_dll.my_cpp_function
 lib_func.argtypes = [ctypes.c_wchar_p, CALLBACK_C_PROTOTYPE]
 lib_func.restype = ctypes.c_int
 
 
 def run():
-    # call function in the my-lib.dll, pass in the callback for communication
+    # call function in my-lib.dll, Callback passed to lib for communication
     callback = Callback()
     returncode = lib_func(u"String arg from in python", callback.c_function)
-    arcpy.AddWarning("RETURN CODE: {} (0=successfull termination, 1=error)".format(
-                          returncode))
-
+    arcpy.AddWarning("RETURN CODE: {} (0=success, 1=error)".format(
+                                                                returncode))
 
 if __name__ == "__main__":
     run()
